3aa52574bfd565e712c100d3d58424e4aa4ea2fa
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-09 15:37:41 -0500

    feat: Verifica as variáveis de ambiente do frontend

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,18 +2,22 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
+require('dotenv').config();
+
+const TIME = 30000;
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3002',
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT || TIME,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL || 'http://localhost:3003',
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT || TIME,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
