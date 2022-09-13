c12fcb8179241608a1d3c4e8de1e6f4a9c44131c
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-13 10:12:33 -0500

    feat: deploy frontend remote-desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
 import React from 'react';
-import StrangerThings from './components/StrangerThings';
-import StrangerThingsDevelpoment from './components/StrangerThingsDevelopment';
+import './App.css';
 
+import StrangerThings from './components/StrangerThings';
+// import StrangerThingsDevelpoment from './components/StrangerThingsDevelopment';
 require('dotenv').config();
 
 function App() {
+  const { REACT_APP_DEVELOPMENT } = process.env;
   return (
     <div className="App">
-      <StrangerThingsDevelpoment />
+      {REACT_APP_DEVELOPMENT === 'development' && (
+        <h1>Em desenvolvimento</h1>
+      )}
       <StrangerThings />
     </div>
   );
--- src/components/StrangerThingsDevelopment.js
+++ src/components/StrangerThingsDevelopment.js
@@ -1,16 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-function StrangerThingsDevelpoment() {
-  const { REACT_APP_DEVELOPMENT } = process.env;
-  return (
-    <div>
-      {REACT_APP_DEVELOPMENT === 'development' && (
-        <h1>Em desenvolvimento</h1>
-      )}
-    </div>
-  );
-}
-
-export default StrangerThingsDevelpoment;
+// function StrangerThingsDevelpoment() {
+//   const { REACT_APP_DEVELOPMENT } = process.env;
+//   return (
+//     <div>
+//       {REACT_APP_DEVELOPMENT === 'development' && (
+//         <h1>Em desenvolvimento</h1>
+//       )}
+//     </div>
+//   );
+// }
+// export default StrangerThingsDevelpoment;
