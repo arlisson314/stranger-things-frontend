e8a48d08ed837f2e1126e65e8e3eeaba03c88a0f
Arlisson N. Costa <arlisson314@gmail.com>
2022-09-13 09:50:59 -0500

    feat: deploy frontend remote-desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
 import './App.css';
-
+import React from 'react';
 import StrangerThings from './components/StrangerThings';
-// import StrangerThingsDevelpoment from './components/StrangerThingsDevelopment';
+import StrangerThingsDevelpoment from './components/StrangerThingsDevelopment';
+
 require('dotenv').config();
 
 function App() {
-  const { REACT_APP_DEVELOPMENT } = process.env;
   return (
     <div className="App">
-      {REACT_APP_DEVELOPMENT === 'development' && (
-        <h1>Em desenvolvimento</h1>
-      )}
+      <StrangerThingsDevelpoment />
       <StrangerThings />
     </div>
   );
